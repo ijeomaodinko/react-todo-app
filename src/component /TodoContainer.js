6547b48a1f31111656cbfa295cb9f59c22d007c7
ijeoma <ijeomaodinko@gmail.com>
2022-08-15 17:17:16 -0400

    Add files

--- src/component /TodoContainer.js
+++ src/component /TodoContainer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import TodosList from "./TodosList";
+import Header from "./Header";
+
 class TodoContainer extends React.Component {
    state = {
     todos: [
@@ -21,10 +24,10 @@ class TodoContainer extends React.Component {
    }
     render () {
     return (
-      <ul>
-        {this.state.todos.map(todo=>(
-        <li>{todo.title}</li>))}
-      </ul>
+        <div>
+         <Header />
+        <TodosList todos={this.state.todos} />
+    </div>
     )
    } 
 }
---
+++ src/component /TodoItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+    render (){
+    return <li>{this.props.todo.title}</li> 
+}
+}
+export default TodoItem
